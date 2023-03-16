76fd5bcb54cffb0d1d8216bd932730a847753e05
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-16 21:11:23 +0300

    Initial commit

--- src/zadanie1_8_1.java
+++ src/zadanie1_8_1.java
@@ -1,5 +1,28 @@
-public class Main {
+import java.util.Scanner;
+
+public class zadanie1_8_1 {
+    //1. Посмотрим и узнаем как можно написать программу проверки чисел гораздо меньше
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /* Было:
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int max;
+        if (x > y) {
+            max = x;
+        } else {
+            max = y;
+        }
+        System.out.println("Max is " + max);
+
+        Стало: */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter: ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int max = x > y ? x : y;
+        System.out.println("Max is " + max);
+        System.out.println("Ответ на задание: см. пример в коде.");
     }
 }
